b87f6bb22bdd02935d359ca549ce427bcf4967aa
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-02-21 14:12:33 -0500

    Update Mark.cpp

--- Mark.cpp
+++ Mark.cpp
@@ -1,9 +1,3 @@
-// Name: Navdeep Virdi
-// Seneca email: nvirdi2@myseneca.ca
-// Student ID: 166485193
-// Date: Feb 21th, 2021
-
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
@@ -12,148 +6,146 @@
 using namespace std;
 namespace sdds 
 {
-	
-	bool Mark::isInvalid()    //if mark is under 0 than there is an invalid state
-    	{ 
-        	return (marks < 0);
-    	}
 
 
-	void Mark::setEmpty() 
-	{
-		marks = 0;
-   	}
-
-	
 	Mark::Mark() 
 	{
-		setEmpty();
+		marks = 0;
 	}
 
-	
-	Mark::Mark(int Marks) 
+
+	Mark::Mark(int mark)
 	{
-		setEmpty();
-			if (Marks >= 0 && Marks <= 100 )
-			{
-				marks = Marks;
-			}
+		if (mark <= 100 && mark >= 0) 
+		{
+			marks = mark;
+		}
+
+		else
+		{
+			marks = -1;
+		}
 	}
 
-	
+
 	Mark::operator int() 
 	{
-		if (isInvalid())
+		if (marks == -1)
 		{
 			return 0;
 		}
 
 		else
-		{	
+		{
 			return marks;
-		}
+		} 
 	}
 
-	
-	Mark::operator char() 
+
+	Mark::operator double() 
 	{
-		if (!isInvalid())
+		if (marks >= 0 && marks < 50)
 		{
-			if (marks >= 80 && marks <= 100)
+			return 0.0;
+		} 
+
+			else if (marks >= 50 && marks < 60)
 			{
-				return 'A';
+				return 1.0;
+			}
+
+			else if (marks >= 60 && marks < 70)
+			{
+				return 2.0;
 			}
 
 			else if (marks >= 70 && marks < 80)
 			{
-				return 'B';
+				return 3.0;
 			}
 
-			else if (marks >= 60 && marks < 70) 
+			else if (marks >= 80 && marks <= 100)
+			{
+				return 4.0;
+			} 
+
+		else
+		{
+			return 0.0;
+		} 
+	}
+
+
+	Mark::operator char() 
+	{
+		if (marks >= 80 && marks <= 100)
+		{
+			return 'A';
+		}
+		
+			else if (marks >= 70 && marks < 80)
+			{
+				return 'B';
+			} 
+
+			else if (marks >= 60 && marks < 70)
 			{
 				return 'C';
-			}
+			} 
 
 			else if (marks >= 50 && marks < 60)
 			{
 				return 'D';
 			} 
 
-			else if (marks > 0 && marks < 50)
+			else if (marks >= 0 && marks < 50)
 			{
-				return 'F';	
-			} 
-		} return 'X';
-	}
+				return 'F';
+			}
 
-
-	Mark::operator double() 
-	{
-		if (!isInvalid())
+		else 
 		{
-			if (marks >= 0 && marks < 50) 
-			{
-				return 0.0;
-			}
-			else if (marks >= 50 && marks < 60)
-			{
-				return 1.0;
-			} 
-			else if (marks >= 60 && marks < 70)
-			{	
-				return 2.0;
-			} 
-			else if (marks >= 70 && marks < 80) 
-			{
-				return 3.0;
-			}
-			else if (marks >= 80 && marks <= 100)
-			{
-				return 4.0;
-			}
-		} return 0.0;
+			return 'X';
+		}
 	}
 
-	
+
 	Mark& Mark::operator+=(int i) 
 	{
-		if (!isInvalid())
+		if (i > 0 && marks + i <= 100)
 		{
-			if (i > 0 && marks + i <= 100 && marks != -1)
+			if (marks != -1)
 			{
 				marks += i;
 			}
+		}
 
-			else 
-			{
-				marks = 0;
-			} 
+		else
+		{
+			marks = 0;
 		} return *this;
 	}
 
-	
+
 	Mark& Mark::operator=(int x) 
 	{
-		if (!isInvalid())
+		if (x >= 0 && x <= 100) 
 		{
-			if (x >= 0 && x <= 100)
-			{
-				marks = x;
-			}
-
-			else
-			{
-				marks = 0;
-			}
-		 } return *this;
+			marks = x;
+		}
+		else 
+		{
+			marks = 0;
+		} return *this;
 	}
 
-	
+
 	int& operator+=(int& l, Mark& Mark) 
 	{
-		if (int(Mark) != -1)
-	 	{
-			 l += int(Mark);
+		if (int(Mark) != -1) 
+		{
+			l += int(Mark);
 		} return l;
 	}
+
 }
